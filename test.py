39beb145c4f799708aae9f024a77d1afbd2718e3
Wyatt Carter - Home <Wyatt@mdcfamily.net>
2013-12-22 21:14:25 -0800

    Don't worry about this file, I was just testing the repository.

---
+++ test.py
@@ -0,0 +1,16 @@
+import time
+count = 0
+while count < 100000000000000000000:
+    count = count+1
+    stringcount = str(count)
+    length = len(stringcount)
+    lengthminus = len(stringcount) - 2
+    if count % 69 == 0:
+        print(str(count))
+        
+    
+        
+    
+          
+        
+        
